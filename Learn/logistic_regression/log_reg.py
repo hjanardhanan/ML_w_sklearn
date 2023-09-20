f3c783d82cd83e42cd0bb46f0b7ac4288df261b8
harij <harishankar088@gmail.com>
2023-09-20 06:07:09 -0700

    remove __init__.py s

--- Learn/logistic_regression/log_reg.py
+++ Learn/logistic_regression/log_reg.py
@@ -1,7 +1,8 @@
 import sys
 import os
 # Hack
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+if os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) not in sys.path :
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from data import data_reader
 df = data_reader.DataReader().pd_read('chip_dataset.csv')

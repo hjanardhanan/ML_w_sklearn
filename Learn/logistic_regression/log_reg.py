9ff3421104e07d489f55675f230e3f6caecde5ff
harij <harishankar088@gmail.com>
2023-09-26 06:31:19 -0700

    move csv location to data

--- Learn/logistic_regression/log_reg.py
+++ Learn/logistic_regression/log_reg.py
@@ -1,5 +1,5 @@
 # Do clustering to :
-# 1. Predict if CPU or GPU (Binary classifier)
+# 1. Predict Type, if CPU or GPU (Binary classifier)
 # 2. Predict vendor  (multi-class classifier)
 # 3. Predict the Foundry (multi-class classifier)
 
@@ -10,5 +10,5 @@ if os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) not in sys.pat
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from data import data_reader
-df = data_reader.DataReader().pd_read('chip_dataset.csv')
+df = data_reader.DataReader().pd_read('chip_dataset_cleaned.csv')
 print(df.head())

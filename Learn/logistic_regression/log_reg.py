67aa61fa9598f9d32f3cd46662bc560365f04168
harij <harishankar088@gmail.com>
2023-09-30 07:04:27 -0700

    introduce classifier base + svm code, first cut

--- Learn/logistic_regression/log_reg.py
+++ Learn/logistic_regression/log_reg.py
@@ -2,53 +2,25 @@
 # 1. Predict Type, if CPU or GPU (Binary classifier)
 # 2. Predict the Foundry (multi-class classifier)
 
-import sys
-import os
-
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn import metrics
-
 # Hack
+import os, sys
 if os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) not in sys.path :
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from data import data_reader
 
-class LogisticReg :
+from classifier_base import cbase
+from sklearn import linear_model
+
+class LogisticReg(cbase.Classifier_Base) :
     def __init__(self, filename, mode) -> None:
-        df = data_reader.DataReader().pd_read(filename)
-        self.clean_n_split(df, mode)
-        self.train(mode)
-        self.evaluate(mode, self.predict(mode))
+        super().__init__(filename, mode)
+        model = self.train(mode)
+        super().evaluate(mode, model)
     
-    def clean_n_split(self, df, mode) :
-        print ("Cleaning and splitting .. ")
-        # Drop multi/binary label based on mode
-        drop_col = 'Foundry' if mode == 'binary' else 'Type'
-        target_cols = 'Foundry' if mode != 'binary' else 'Type'
-        df.drop(drop_col, axis = 1, inplace = True)
-        # Training data columns
-        train_cols = list(set(df.columns) - set([target_cols]))
-        self.X_train, self.X_test, self.y_train, self.y_test = \
-            train_test_split(df.loc[:, train_cols], df.loc[:, target_cols], \
-                                random_state=203, test_size=0.2, shuffle=True)
-
     def train(self, mode) :
         print ("Training .. ")
-        self.model = linear_model.LogisticRegression(random_state=22).fit(self.X_train, self.y_train)
-    
-    def predict(self, mode) :
-        print ("Predicting .. ")
-        y_pred = self.model.predict(self.X_test)
-        return y_pred
-    
-    def evaluate(self, mode, y_pred) :
-        print ("Evaluating .. ")
-        report = metrics.classification_report(self.y_test, y_pred)
-        print (report)
-        scores = cross_val_score(self.model, self.X_train, self.y_train, cv = 5)
-        print ("CV scores = ",scores)
-    
+        model = linear_model.LogisticRegression(random_state=22).fit(self.X_train, self.y_train)
+        return model
+   
 # Test 
 # LogisticReg('chip_dataset_cleaned.csv', 'binary')
 LogisticReg('chip_dataset_cleaned.csv', 'multi')

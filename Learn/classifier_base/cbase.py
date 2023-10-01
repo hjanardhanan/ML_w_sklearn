ccd1ad97104bdd17eed981d67a6759933194f3f9
harij <harishankar088@gmail.com>
2023-10-01 06:38:27 -0700

    missing adds; precision score wrapper; knn skeleton

---
+++ Learn/classifier_base/cbase.py
@@ -0,0 +1,36 @@
+import sys
+import os
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn import metrics
+
+# Hack
+if os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) not in sys.path :
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from data import data_reader
+
+class Classifier_Base :
+    def __init__(self, filename, mode) -> None:
+         df = data_reader.DataReader().pd_read(filename)
+         self.clean_n_split(df, mode)
+
+    def clean_n_split(self, df, mode) :
+        print ("Cleaning and splitting .. ")
+        # Drop multi/binary label based on mode
+        drop_col = 'Foundry' if mode == 'binary' else 'Type'
+        target_cols = 'Foundry' if mode != 'binary' else 'Type'
+        df.drop(drop_col, axis = 1, inplace = True)
+        # Training data columns
+        train_cols = list(set(df.columns) - set([target_cols]))
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+            train_test_split(df.loc[:, train_cols], df.loc[:, target_cols], \
+                                random_state=203, test_size=0.2, shuffle=True)
+    
+    def evaluate(self, mode, model) :
+        print ("Evaluate .. ")
+        y_pred = model.predict(self.X_test)
+        report = metrics.classification_report(self.y_test, y_pred)
+        print(report)
+        scores = cross_val_score(model, self.X_train, self.y_train, cv = 5)
+        print("CV score = ", scores)
+
+
---
+++ Learn/svm/svm.py
@@ -0,0 +1,42 @@
+# Study different kernels : linear, polynomial, rbf
+# Choose optimal C and gamma using grid search
+# Hack
+import os,sys
+if os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) not in sys.path :
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from sklearn import svm
+from classifier_base import cbase
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+from sklearn.metrics import precision_score, make_scorer
+
+class Svc(cbase.Classifier_Base) :
+    def __init__(self, filename, mode) -> None:
+        super().__init__(filename, mode)
+        model = self.train(mode)
+        super().evaluate(mode, model)
+
+    def train(self, mode) :
+        print ("Training .. ")
+        kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+        degrees = list(range(1, 4))
+        Cs = list(np.arange(0.5, 10.5, 1.0))
+        parameters =    {
+                            'kernel' : kernels,
+                            'degree' : degrees,
+                            'C' : Cs
+                        }
+        # precision needs more arguments which cannot be directly passed in 
+        precision_wrapper = make_scorer(precision_score, average='micro')#, zero_division=np.nan)
+        svc = GridSearchCV(estimator=svm.SVC(),
+                            param_grid=parameters,
+                                scoring='precision' if mode == 'binary' else precision_wrapper )
+        svc.fit(self.X_train, self.y_train)
+        print(svc.best_params_)
+        print(svc.best_score_)
+        print(svc.best_estimator_)
+        return svc.best_estimator_ 
+
+
+Svc('chip_dataset_cleaned.csv', 'multi')

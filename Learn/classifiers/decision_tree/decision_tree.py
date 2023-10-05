35d2075f91a62069a3d93a43254c804b1955ca14
harij <harishankar088@gmail.com>
2023-10-04 17:20:25 -0700

    post pruning alpha for decision tree

--- Learn/classifiers/decision_tree/decision_tree.py
+++ Learn/classifiers/decision_tree/decision_tree.py
@@ -4,6 +4,7 @@ if os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) not in sys.pat
 
 from sklearn import tree
 from classifier_base.cbase import Classifier_Base
+import matplotlib.pyplot as plt
 
 class DTree(Classifier_Base) :
     def __init__(self, filename, mode) -> None:
@@ -24,7 +25,29 @@ class DTree(Classifier_Base) :
                                           random_state=33,
                                           min_impurity_decrease=0.0,
                                           class_weight=None,
-                                          ccp_alpha=0.03).fit(self.X_train, self.y_train)
+                                          ccp_alpha=0.0)
+        # Find ideal post pruning value for alpha
+        path = dtc.cost_complexity_pruning_path(self.X_train, self.y_train)
+        ccp_alphas, impurities = path.ccp_alphas, path.impurities
+        global_score = None
+        for ccp_alpha in ccp_alphas :
+            dtc_local = tree.DecisionTreeClassifier(criterion='entropy',
+                                          splitter='best',
+                                          max_depth=None,
+                                          min_samples_split=3, # smallest label has 2 observations
+                                          min_samples_leaf=1, # 
+                                          min_weight_fraction_leaf=0,
+                                          max_features=7,
+                                          max_leaf_nodes=None,
+                                          random_state=33,
+                                          min_impurity_decrease=0.0,
+                                          class_weight=None,
+                                          ccp_alpha=ccp_alpha).fit(self.X_train, self.y_train)
+            sc = dtc_local.score(self.X_train, self.y_train)
+            if global_score is None or \
+                    sc > global_score :
+                dtc = dtc_local
+                global_score = sc
         return dtc
         
 # DTree('chip_dataset_cleaned.csv', 'binary')

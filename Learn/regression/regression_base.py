b1026a02162869526fadada6a6fb45a0ab3f4ce0
harij <harishankar088@gmail.com>
2023-10-21 06:28:12 -0700

    get feature names from transformer before writing

--- Learn/regression/regression_base.py
+++ Learn/regression/regression_base.py
@@ -6,15 +6,16 @@ from sklearn import metrics
 class RegressionBase :
     def __init__(self, filename) :
         df = pd.read_csv('./Learn/regression/data/' + filename)
-        self.test_train_split(df)
         self.target_col = ['price']
         self.train_cols = list(set(df.columns) - set(self.target_col))
+        self.test_train_split(df)
 
     def test_train_split(self, df) :
         print ("Test & train split ..")
-        self.X_train, self.X_test, self.y_train, self.y_test = \
-            train_test_split(df.loc[:, self.train_cols], df.loc[:, self.target_col], \
-                                random_state=203, test_size=0.2, shuffle=True)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split \
+                          (df.loc[:, self.train_cols], \
+                            df.loc[:, self.target_col], \
+                              random_state=203, test_size=0.2, shuffle=True)
 
     def evaluate(self, model) :
         print ("Evaluating regression model ..")
